949e168f30f09a6c632c822deefe6b144378194f
ItssJxstnDe <127620206+ItssJxstnDe@users.noreply.github.com>
2024-07-07 10:58:25 +0200

    Delete src directory

--- src/components/Light/Row/index.js
+++
@@ -1,7 +0,0 @@
-export default function LightRow({ index, currentIndex, children }) {
-	return (
-		<div className={`flex space-x-1 ${index === currentIndex && "bg-white/10"}`}>
-			{ children }
-		</div>
-	)
-}
